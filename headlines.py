52dd55c54d6284f1df4bdfa3ed1017f34579d8b8
KillLoGiC <cristian_eder@hotmail.com>
2017-11-13 13:24:20 -0700

    GET request

--- headlines.py
+++ headlines.py
@@ -1,6 +1,7 @@
 import feedparser #jinja
 from flask import Flask
 from flask import render_template
+from flask import request #Get request
 """Takes a Jinja template as input and produces pure HTML, capable of
 being read by any browser, as the output
 """
@@ -12,10 +13,18 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 @app.route("/")
-@app.route("/<publication>")
-#Creates an argument called publication, which we need to add as a parameter 
-#of the function directly, below the route
 def get_news(publication="bbc"):
+    query = request.args.get("publication")
+    """
+    This is useful becase the GET arguments that our user passes along as
+    part of a request are automatically available in request.args, from which
+    we can access key-value. 
+    """
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
+
     feed = feedparser.parse(RSS_FEEDS[publication])
     return render_template("home.html", articles=feed['entries'])
 
