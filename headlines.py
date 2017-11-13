c61751d26047ce420bafc6843facff1b25bc3b37
KillLoGiC <cristian_eder@hotmail.com>
2017-11-13 11:24:33 -0700

    Jinja Objects

--- headlines.py
+++ headlines.py
@@ -5,37 +5,20 @@ from flask import render_template
 being read by any browser, as the output
 """
 app = Flask(__name__)
-"""
-Create a instance of the Flask object using our module name as parameter,
-Flask use this to resolve resources, and in complex cases one can use other that __name__ here
-"""
-RSS_FEEDS = {
-    'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
-    'cnn':'http://rss.cnn.com/rss/edition.rss',
-    'fox':'http://feeds.foxnews.com/foxnews/latest',
-    'iol':'http://www.iol.co.za/cmlink/1.640'}
+
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 @app.route("/")
-
 @app.route("/<publication>")
-
-#Creates an argument called publication, which we need to add as a parameter of the function directly,
-#below the route
-
+#Creates an argument called publication, which we need to add as a parameter 
+#of the function directly, below the route
 def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return render_template("home.html",
-                           tittle=first_article.get("tittle"),
-                           published=first_article.get("published"),
-                           summary=first_article.get("summary"))
-"""
-Render_template function takes the filename of the template as its first
-argument, and can then take an arbitrary number of named variables as subsequent
-arguments. The data in each of the variables will be available to the template, using
-the variable name
-"""
-if __name__ == '__main__':
+    return render_template("home.html", articles=feed['entries'])
+
+if __name__ == "__main__":
     #If our application is run directly kicks off development server on our local machine
     app.run(port=5000, debug=True)
-
