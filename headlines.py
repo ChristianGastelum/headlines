20b4af99fb20d4952b2dc4c0f4243b53555dfade
KillLoGiC <cristian_eder@hotmail.com>
2017-11-13 10:58:09 -0700

    Jinja  dynamic data

--- headlines.py
+++ headlines.py
@@ -1,6 +1,9 @@
-import feedparser
+import feedparser #jinja
 from flask import Flask
-#imports flask from package flask
+from flask import render_template
+"""Takes a Jinja template as input and produces pure HTML, capable of
+being read by any browser, as the output
+"""
 app = Flask(__name__)
 """
 Create a instance of the Flask object using our module name as parameter,
@@ -15,23 +18,24 @@ RSS_FEEDS = {
 @app.route("/")
 
 @app.route("/<publication>")
-"""
-Creates an argument called publication, which we need to add as a parameter of the function directly,
-below the route
-"""
-def get_news(publication="cnn"):
+
+#Creates an argument called publication, which we need to add as a parameter of the function directly,
+#below the route
+
+def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    return """<html>
-        <body>
-            <h1>headlines</h1>
-            <b>{0}</b> <br/>
-            <i>{1}</i> <br/>
-            <p>{2}</p> <br/>
-        </body>
-    </html>""".format(first_article.get("tittle"), first_article.get("published"), 
-    first_article.get("summary"))
- 
+    return render_template("home.html",
+                           tittle=first_article.get("tittle"),
+                           published=first_article.get("published"),
+                           summary=first_article.get("summary"))
+"""
+Render_template function takes the filename of the template as its first
+argument, and can then take an arbitrary number of named variables as subsequent
+arguments. The data in each of the variables will be available to the template, using
+the variable name
+"""
 if __name__ == '__main__':
     #If our application is run directly kicks off development server on our local machine
     app.run(port=5000, debug=True)
+
