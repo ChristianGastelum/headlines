9d296d7e5e4dd61eafa40aad26a852ac37447b3a
KillLoGiC <cristian_eder@hotmail.com>
2017-11-13 09:30:22 -0700

    dinamic routing

--- headlines.py
+++ headlines.py
@@ -6,22 +6,29 @@ app = Flask(__name__)
 Create a instance of the Flask object using our module name as parameter,
 Flask use this to resolve resources, and in complex cases one can use other that __name__ here
 """
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+RSS_FEEDS = {
+    'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
+    'cnn':'http://rss.cnn.com/rss/edition.rss',
+    'fox':'http://feeds.foxnews.com/foxnews/latest',
+    'iol':'http://www.iol.co.za/cmlink/1.640'}
 
 @app.route("/")
 
-def get_news():
-    feed = feedparser.parse(BBC_FEED)
+@app.route("/<publication>")
+
+def get_news(publication="cnn"):
+    feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
     return """<html>
         <body>
-            <h1> BBC headlines</h1>
+            <h1>headlines</h1>
             <b>{0}</b> <br/>
             <i>{1}</i> <br/>
             <p>{2}</p> <br/>
         </body>
-    </html>""".format(first_article.get("tittle"), first_article.get("published"), first_article.get("summary"))
-    
+    </html>""".format(first_article.get("tittle"), first_article.get("published"), 
+    first_article.get("summary"))
+ 
 if __name__ == '__main__':
     #If our application is run directly kicks off development server on our local machine
     app.run(port=5000, debug=True)
